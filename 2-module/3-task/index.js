8e543cfbd63407c6c510f4dc0105fa9908b80e8e
Yana <yanamatyunina@yandex.ru>
2023-09-21 19:01:26 +0300

    calculator

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,20 @@
 let calculator = {
-  // ваш код
+  a : 0,
+  b : 0,
+
+  read: function(val1,val2) {
+    this.a = val1;
+    this.b = val2;
+  },
+
+  sum : function() {
+    return this.a+this.b;
+  },
+
+  mul: function() {
+    return this.a*this.b;
+  }
+
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
