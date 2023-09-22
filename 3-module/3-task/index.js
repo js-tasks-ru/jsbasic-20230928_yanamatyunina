9e4cf8db6e19e4ac87ce503f8ed95e81227c8103
Yana <yanamatyunina@yandex.ru>
2023-09-22 14:52:38 +0300

    camelize

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,14 @@
 function camelize(str) {
-  // ваш код...
+  let res='';
+
+  let arr = str.split("");
+  while(arr.indexOf('-')>=0) {
+    let sym = arr.indexOf("-");
+    arr[sym+1] = arr[sym+1].toUpperCase();
+    arr.splice(sym,1);
+
+  }
+  res = arr.join("");
+  return res;
+
 }
