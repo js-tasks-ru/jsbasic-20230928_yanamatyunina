828977254326d06b6720505bd25e25aef16f9159
Yana <yanamatyunina@yandex.ru>
2023-09-22 13:28:02 +0300

    filter array

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,9 @@
 function filterRange(arr, a, b) {
-  // ваш код...
+  let res = new Array();
+  for(let i=0 ; i<arr.length ; i++) {
+    if(arr[i]>=a && arr[i]<=b) {
+      res.push(arr[i]);
+    }
+  }
+  return res;
 }
