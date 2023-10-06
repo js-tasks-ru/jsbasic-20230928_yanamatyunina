054f7f48b1a09487f65b6a72bf5440b11737de5d
Yana <yanamatyunina@yandex.ru>
2023-10-06 18:41:31 +0300

    highlight

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,24 @@
 function highlight(table) {
-  // ваш код...
+  for(let i=1 ; i<table.rows.length ; i++) {
+
+    if(table.rows[i].cells[3].dataset.available=='true') {
+      table.rows[i].classList.add('available');
+    } else if(table.rows[i].cells[3].dataset.available==undefined) {
+      table.rows[i].classList.add('hidden');
+      table.rows[i].setAttribute('hidden', true)
+    } else if(table.rows[i].cells[3].dataset.available=='false'){
+      table.rows[i].classList.add('unavailable');
+    }
+
+    if(table.rows[i].cells[2].innerHTML =='m') {
+      table.rows[i].classList.add('male');
+    } else {
+      table.rows[i].classList.add('female');
+    }
+
+    if(+table.rows[i].cells[1].innerHTML <18) {
+      table.rows[i].style="text-decoration: line-through;"
+    }
+
+  }
 }
