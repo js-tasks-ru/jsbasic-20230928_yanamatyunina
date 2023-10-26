e88e197ffc59d0b5b43182f6ad4b8fa18c121e10
Yana <yanamatyunina@yandex.ru>
2023-10-26 22:44:56 +0300

    remove row table

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -1,18 +1,36 @@
-/**
- * Компонент, который реализует таблицу
- * с возможностью удаления строк
- *
- * Пример одного элемента, описывающего строку таблицы
- *
- *      {
- *          name: 'Ilia',
- *          age: 25,
- *          salary: '1000',
- *          city: 'Petrozavodsk'
- *      }
- *
- */
 export default class UserTable {
+
+   
+
   constructor(rows) {
+
+    this.rows=rows;
+    this.str = "<thead><th>Имя</th><th>Возраст</th><th>Зарплата</th><th>Город</th><th></th></tr></thead><tbody>"
+
+    this.elem = document.createElement('table');
+    
+    for(let i=0 ; i<this.rows.length ; i++) {
+      this.str += "<tr>";
+      let objRow = this.rows[i];
+      for(let key in objRow) {
+        this.str += `<td>${objRow[key]}</td>`;
+      }
+      this.str += "<td><button>X</button></td></tr>";
+    }
+    this.str += "</tbody>";
+
+    this.elem.innerHTML = this.str;
+    this.elem.addEventListener('click', (event) => this.removeRow(event));
+  }
+
+  removeRow(event) {
+    if (event.target.tagName == 'BUTTON') {
+      let tr = event.target.closest('tr');
+      tr.remove();
+    }
+
+    
   }
 }
+
+
