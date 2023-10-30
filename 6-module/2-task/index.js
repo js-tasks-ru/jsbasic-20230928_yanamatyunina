f875b20dcdc40e4c22cc3610c66635c1ec3f1893
Yana <yanamatyunina@yandex.ru>
2023-10-30 16:04:10 +0300

    creating product card

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,40 @@
+import createElement from '../../assets/lib/create-element.js';
+
 export default class ProductCard {
   constructor(product) {
+    this.product=product;
+    
+    this.elem = createElement(`
+      <div class="card">
+        <div class="card__top">
+            <img class="card__image" alt="product">
+            <span class="card__price"></span>
+        </div>
+        <div class="card__body">
+            <div class="card__title"></div>
+            <button type="button" class="card__button">
+                <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+            </button>
+        </div>
+    </div>
+  `  )
+
+    this.elem.querySelector(".card__price").innerHTML='€'+this.product.price.toFixed(2);
+
+    let path ="/assets/images/products/";
+    this.elem.querySelector(".card__image").setAttribute('src',path+this.product.image);
+    
+    this.elem.querySelector(".card__title").innerHTML = this.product.name;
+
+    this.elem.onclick = (event) => this.onClick(event);
   }
+
+  onClick(event) {
+    this.elem.dispatchEvent(new CustomEvent("product-add", {
+      detail: this.product.id,
+      bubbles: true
+    }));
+  }
+ 
 }
+
